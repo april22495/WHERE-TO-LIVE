2de7b424d42eb83945702ed48a62ec2e903735b2
aprilee <smileeesun@snu.ac.kr>
2017-06-14 16:00:38 +0900

    radar chart draw in rating.js,(xml error yes)

--- js/rating.js
+++ js/rating.js
@@ -599,3 +599,35 @@ $.getScript("js/radar-chart.js", function(){
 });
 
 */
+
+$.getScript("js/radar-chart.js", function(){
+	RadarChart.defaultConfig.color = function() {};
+	RadarChart.defaultConfig.radius = 3;
+
+	var data = [
+	{
+		className: 'germany', // optional can be used for styling
+		axes: [
+		{axis: "strength", value: 13}, 
+		{axis: "intelligence", value: 6}, 
+		{axis: "charisma", value: 5},  
+		{axis: "dexterity", value: 9},  
+		{axis: "luck", value: 2}
+		]
+	},
+	{
+		className: 'argentina',
+		axes: [
+		{axis: "strength", value: 6}, 
+		{axis: "intelligence", value: 7}, 
+		{axis: "charisma", value: 10},  
+		{axis: "dexterity", value: 13},  
+		{axis: "luck", value: 9}
+		]
+	}
+	];
+
+	RadarChart.defaultConfig.levelTick = true;
+	RadarChart.draw(".chart-container", data);
+
+});
